d8ce4b725d3a5342f3d6b90e95ab33a2a7b73fd6
Patricia Nadine Argueza <patricianadineargueza@Patricias-MacBook-Pro.local>
2026-05-26 12:29:40 -0700

    add site name

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Marina Bay Media';
-export const SITE_DESCRIPTION = 'Welcome to the Marina Bay Media website!';
+export const SITE_TITLE = 'Tease';
+export const SITE_DESCRIPTION = 'Welcome to the Tease website!';
